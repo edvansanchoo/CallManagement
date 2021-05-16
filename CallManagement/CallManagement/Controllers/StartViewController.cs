56b1a8936e3f58fb86de6a2f2b52014fd3e58cb1
Edvan Sancho <edvansanchoo@gmail.com>
2021-05-16 13:48:03 -0400

    The first page

---
+++ CallManagement/CallManagement/Controllers/StartViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallManagement.Controllers
+{
+    public class StartViewController : Controller
+    {
+        // GET: StartView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
