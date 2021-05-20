56c807e402bf655135d0400189af867ef87a49df
Kevin Marinho <kevindmarinho4321@gmail.com>
2021-05-19 22:49:16 -0300

    Controller Edit and Close

---
+++ CallManagement/CallManagement/Controllers/EditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallManagement.Controllers
+{
+    public class EditController : Controller
+    {
+        // GET: Edit
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
